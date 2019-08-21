41453ac406e6981f4bbbf554558281292b1eae04
NtBat <na.than.fjv@gmail.com>
2019-08-21 00:16:36 -0300

    Criado script para mudar a avaliação

--- js/main/script.js
+++ js/main/script.js
@@ -1,18 +1,34 @@
-const $heart = document.querySelector(".-heart");
+{
+  const $heart = document.querySelector(".-heart");
 
-$heart.addEventListener("click", handleClick);
+  $heart.addEventListener("click", handleClick);
 
-function handleClick() {
-  $heart.classList.toggle("-active");
+  function handleClick() {
+    $heart.classList.toggle("-active");
+  }
 }
 
-const $cart = document.querySelector(".button-store.-second");
-const $qtdCart = document.querySelector(".qtd-cart");
-let $valueCart = 10;
+{
+  const $cart = document.querySelector(".button-store.-second");
+  const $qtdCart = document.querySelector(".qtd-cart");
+  let $valueCart = 10;
 
-$cart.addEventListener("click", handleCart);
+  $cart.addEventListener("click", handleCart);
 
-function handleCart() {
-  $valueCart++;
-  $qtdCart.textContent = `(${$valueCart})`;
+  function handleCart() {
+    $valueCart++;
+    $qtdCart.textContent = `(${$valueCart})`;
+  }
+}
+
+{
+  const $stars = document.querySelectorAll(".star-list");
+
+  $stars.forEach(function ($star, key) {
+    $star.addEventListener("click", handleStar);
+  });
+
+  function handleStar() {
+    this.classList.toggle("-active");
+  }
 }
